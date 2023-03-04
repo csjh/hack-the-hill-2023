ab5e055def5b661be262f76c2c5b4dbb42fd0735
csjh <cormick.holland@gmail.com>
2023-03-04 14:26:52 -0500

    better base level implementation of tom's mockup

--- App.js
+++ App.js
@@ -9,13 +9,31 @@ import {
   ImageBackground,
 } from "react-native";
 import { Camera } from "expo-camera";
-import { Constants, Svg } from "expo";
+import Svg, { Path } from "react-native-svg";
+
 let camera;
+
+const PlayButton = () => {
+  return (
+    <Svg width="100%" height="100%" viewBox="0 0 24 24">
+      <Path fill="white" d="M8,5.14V19.14L19,12.14L8,5.14Z" />
+    </Svg>
+  );
+};
+
+const PauseButton = () => {
+  return (
+    <Svg width="100%" height="100%" viewBox="0 0 24 24">
+      <Path fill="white" d="M14,19H18V5H14M6,19H10V5H6V19Z" />
+    </Svg>
+  );
+};
 
 export default function App() {
   const [startCamera, setStartCamera] = React.useState(false);
   const [previewVisible, setPreviewVisible] = React.useState(false);
   const [capturedImage, setCapturedImage] = React.useState(null);
+  const [paused, setPaused] = React.useState(false);
 
   const requestPermissions = async () => {
     const { status } = await Camera.requestCameraPermissionsAsync();
@@ -27,106 +45,57 @@ export default function App() {
     }
   };
   const captureImage = async () => {
-    const photo = await camera.takePictureAsync();
-    console.log(photo);
-    setPreviewVisible(true);
-    setCapturedImage(photo);
+    if (paused) {
+      setPaused(false);
+    } else {
+      const photo = await camera.takePictureAsync();
+      console.log(photo);
+      setPreviewVisible(true);
+      setCapturedImage(photo);
+      setPaused(true);
+    }
   };
   return (
     <View style={styles.container}>
-      {startCamera ? (
+      <View
+        style={{
+          flex: 1,
+          width: "100%",
+        }}
+      >
+        {paused ? (
+          <CapturedImage photo={capturedImage} />
+        ) : (
+          <Camera
+            type={Camera.Constants.Type.back}
+            style={{ flex: 10 }}
+            ref={(r) => {
+              camera = r;
+            }}
+          />
+        )}
         <View
           style={{
-            flex: 1,
-            width: "100%",
-          }}
-        >
-          {previewVisible && capturedImage ? (
-            <CapturedImage photo={capturedImage} />
-          ) : (
-            <>
-              <Camera
-                type={Camera.Constants.Type.back}
-                style={{ flex: 10 }}
-                ref={(r) => {
-                  camera = r;
-                }}
-              />
-              <View
-                style={{
-                  flex: 1.5,
-                  backgroundColor: "black",
-                  alignItems: "center",
-                }}
-              >
-                <TouchableOpacity
-                  onPress={captureImage}
-                  style={{
-                    borderRadius: 100,
-                    borderColor: "white",
-                    borderWidth: 4,
-                    width: "20%",
-                    flex: 0.85,
-                    top: "7.5%",
-                  }}
-                >
-                  <Svg height={100} width={100}>
-                    <Svg.Circle
-                      cx={50}
-                      cy={50}
-                      r={45}
-                      strokeWidth={2.5}
-                      stroke="#e74c3c"
-                      fill="#f1c40f"
-                    />
-                    <Svg.Rect
-                      x={15}
-                      y={15}
-                      width={70}
-                      height={70}
-                      strokeWidth={2}
-                      stroke="#9b59b6"
-                      fill={toggle ? "#3498db" : "#9b59b6"}
-                    />
-                  </Svg>
-                </TouchableOpacity>
-              </View>
-            </>
-          )}
-        </View>
-      ) : (
-        <View
-          style={{
-            flex: 1,
-            backgroundColor: "#fff",
-            justifyContent: "center",
+            flex: 1.5,
+            backgroundColor: "black",
             alignItems: "center",
           }}
         >
           <TouchableOpacity
-            onPress={requestPermissions}
+            onPress={captureImage}
             style={{
-              width: 130,
-              borderRadius: 4,
-              backgroundColor: "#14274e",
-              flexDirection: "row",
-              justifyContent: "center",
-              alignItems: "center",
-              height: 40,
+              borderRadius: 100,
+              borderColor: "white",
+              borderWidth: 4,
+              width: "20%",
+              flex: 0.85,
+              top: "7.5%",
             }}
           >
-            <Text
-              style={{
-                color: "#fff",
-                fontWeight: "bold",
-                textAlign: "center",
-              }}
-            >
-              Take picture
-            </Text>
+            {paused ? <PlayButton /> : <PauseButton />}
           </TouchableOpacity>
         </View>
-      )}
+      </View>
 
       <StatusBar style="auto" />
     </View>
@@ -143,12 +112,11 @@ const styles = StyleSheet.create({
 });
 
 const CapturedImage = ({ photo }) => {
-  console.log("sdsfds", photo);
   return (
     <View
       style={{
         backgroundColor: "transparent",
-        flex: 1,
+        flex: 10,
         width: "100%",
         height: "100%",
         borderColor: "#fff",
@@ -159,7 +127,7 @@ const CapturedImage = ({ photo }) => {
         source={{ uri: photo && photo.uri }}
         style={{
           flex: 1,
-          height: "75%",
+          height: "100%",
         }}
       />
     </View>
