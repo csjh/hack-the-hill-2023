74377dbe9f2237553443654eb5bf4f5a15bb20e8
Ze Feng Wang <zefengwang613@gmail.com>
2023-03-04 17:37:31 -0500

    Obj detection and image receiving

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,29 @@
 import flask
+import base64
+from io import BytesIO
+from PIL import Image
+from imageai.Detection import ObjectDetection
+
+detector = ObjectDetection()
+detector.setModelTypeAsYOLOv3()
+detector.setModelPath("../yolov3.pt")
+detector.loadModel()
 
 app = flask.Flask(__name__)
 
-@app.route('/')
+
+@app.route('/', methods=['POST'])
 def index():
-    return 'Hello World'
+    image = flask.request.json['image']
+    image = base64.b64decode(image)
+    image = BytesIO(image)
+    detections = detector.detectObjectsFromImage(
+        input_image=Image.open(image), output_image_path="imagenew.jpg", minimum_percentage_probability=30)
+
+    result = ""
+    for eachObject in detections:
+        result += eachObject["name"] + " : " + \
+            str(eachObject["percentage_probability"]) + \
+            " : " + str(eachObject["box_points"])
+        result += "\n--------------------------------\n"
+    return result
--- frontend/App.js
+++ frontend/App.js
@@ -1,10 +1,114 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Alert,
+  ImageBackground,
+  TouchableWithoutFeedback,
+} from "react-native";
+import { Camera } from "expo-camera";
+import Svg, { Path } from "react-native-svg";
+import FileSystem from "expo-file-system";
+
+const PlayButton = () => {
+  return (
+    <Svg width="100%" height="100%" viewBox="0 0 24 24">
+      <Path fill="white" d="M8,5.14V19.14L19,12.14L8,5.14Z" />
+    </Svg>
+  );
+};
+
+const PauseButton = () => {
+  return (
+    <Svg width="100%" height="100%" viewBox="0 0 24 24">
+      <Path fill="white" d="M14,19H18V5H14M6,19H10V5H6V19Z" />
+    </Svg>
+  );
+};
 
 export default function App() {
+  const [capturedImage, setCapturedImage] = useState(null);
+  const [paused, setPaused] = useState(false);
+
+  const requestPermissions = async () => {
+    const { status } = await Camera.requestCameraPermissionsAsync();
+    console.log(status);
+    if (status !== "granted") {
+      Alert.alert("Camera access is required to use Colour Sense!");
+    }
+  };
+  const captureImage = async () => {
+    if (paused) {
+      setPaused(false);
+    } else {
+      await requestPermissions();
+      const photo = await this.camera.takePictureAsync({ skipProcessing: true });
+      console.log(photo);
+      setCapturedImage(photo);
+      setPaused(true);
+    }
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View
+        style={{
+          flex: 1,
+          width: "100%",
+        }}
+      >
+        <TouchableWithoutFeedback
+          onPress={(e) =>
+            console.log(
+              `x is ${e.nativeEvent.locationX}, y is ${e.nativeEvent.locationY}`
+            )
+          }
+        >
+          <CapturedImage photo={capturedImage} show={paused} />
+        </TouchableWithoutFeedback>
+        <TouchableWithoutFeedback
+          onPress={(e) =>
+            console.log(
+              `x is ${e.nativeEvent.locationX}, y is ${e.nativeEvent.locationY}`
+            )
+          }
+        >
+          <Camera
+            type={Camera.Constants.Type.back}
+            style={{ flex: 10, display: paused ? "none" : "show" }}
+            ref={(r) => {
+              this.camera = r;
+            }}
+          />
+        </TouchableWithoutFeedback>
+        <View
+          style={{
+            flex: 1.5,
+            backgroundColor: "black",
+            alignItems: "center",
+          }}
+        >
+          <TouchableOpacity
+            onPress={(e) => {
+              captureImage();
+            }}
+            style={{
+              borderRadius: 100,
+              borderColor: "white",
+              borderWidth: 4,
+              width: "20%",
+              flex: 0.7,
+              top: "7.5%",
+            }}
+          >
+            {paused ? <PlayButton /> : <PauseButton />}
+          </TouchableOpacity>
+        </View>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +117,32 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+
+const CapturedImage = ({ photo, show }) => {
+  return (
+    <View
+      style={{
+        backgroundColor: "transparent",
+        display: show ? "flex" : "none",
+        flex: 10,
+        width: "100%",
+        height: "100%",
+        borderColor: "#fff",
+        borderWidth: 1,
+      }}
+    >
+      <ImageBackground
+        source={{ uri: photo && photo.uri }}
+        style={{
+          flex: 1,
+          height: "100%",
+        }}
+      />
+    </View>
+  );
+};
