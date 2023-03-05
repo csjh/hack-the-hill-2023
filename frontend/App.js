41ec4ffbf6586bff02ed04ba678968a5653f9d99
csjh <cormick.holland@gmail.com>
2023-03-05 03:02:21 -0500

    dot work

--- frontend/App.js
+++ frontend/App.js
@@ -47,11 +47,24 @@ export default function App() {
     } else {
       await requestPermissions();
       const photo = await this.camera.takePictureAsync({
-        skipProcessing: true, base64: true, quality: 0
+        base64: true
       });
-      console.log(photo.base64.length);
-      setCapturedImage(photo);
-      setPaused(true);
+      console.log("photo ", photo.height, photo.width)
+      console.log("i'm here")
+      fetch("http://127.0.0.1:5000/get_all_pixels", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          image: photo.base64,
+        }),
+      })
+        .then((r) => r.json())
+        .then((r) => {
+          setPixels(r);
+          console.log(r);
+          setCapturedImage(photo);
+          setPaused(true);
+        });
     }
   };
   return (
@@ -62,7 +75,13 @@ export default function App() {
           width: "100%",
         }}
       >
-        <CapturedImage photo={capturedImage} show={paused} capturedImage={capturedImage} setPixels={setPixels} />
+        <CapturedImage
+          photo={capturedImage}
+          show={paused}
+          capturedImage={capturedImage}
+          pixels={pixels}
+          setPixels={setPixels}
+        />
         <Camera
           type={Camera.Constants.Type.back}
           style={{ flex: 10, display: paused ? "none" : "show" }}
@@ -109,43 +128,67 @@ const styles = StyleSheet.create({
   },
 });
 
-const CapturedImage = ({ photo, show, capturedImage, setPixels }) => {
+const CapturedImage = ({ photo, show, capturedImage, pixels, setPixels }) => {
+  const [circlePosition, setCirclePosition] = useState(null);
+
+  const handlePress = async (event) => {
+    const { pageX = 0, pageY = 0 } = event.nativeEvent;
+    console.log("pageX ", pageX, "pageY ", pageY);
+    console.log(
+      "pixels ",
+      pixels[Math.round(pageY)],
+      pixels.length,
+      pixels[0].length
+    );
+    console.log(
+      `rgb(${pixels[Math.round(pageY)][Math.round(pageX)][0]}, ${
+        pixels[Math.round(pageY)][Math.round(pageX)][1]
+      }, ${pixels[Math.round(pageY)][Math.round(pageX)][2]})`
+    );
+    setCirclePosition({ x: Math.round(pageX), y: Math.round(pageY) });
+  };
+
   return (
-    <TouchableWithoutFeedback
-      onPress={async (e) => {
-        console.log("in the request!!!");
-        const { locationX, locationY } = e.nativeEvent;
-        const pixelColors = await fetch("http://172.20.10.9:5000/get_pixel", {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({
-            pixel: [locationX, locationY],
-            image: capturedImage.base64,
-          }),
-        }).then((r) => r.text());
-        console.log(pixelColors);
-        setPixels(pixelColors);
+    <View
+      style={{
+        backgroundColor: "transparent",
+        display: show ? "flex" : "none",
+        flex: 10,
+        width: "100%",
+        height: "100%",
+        borderColor: "#fff",
+        borderWidth: 1,
       }}
     >
-      <View
-        style={{
-          backgroundColor: "transparent",
-          display: show ? "flex" : "none",
-          flex: 10,
-          width: "100%",
-          height: "100%",
-          borderColor: "#fff",
-          borderWidth: 1,
-        }}
-      >
-        <ImageBackground
-          source={{ uri: photo && photo.uri }}
-          style={{
-            flex: 1,
-            height: "100%",
-          }}
-        />
-      </View>
-    </TouchableWithoutFeedback>
+      <TouchableWithoutFeedback onPress={handlePress}>
+        <View style={{ flex: 1 }}>
+          {circlePosition && (
+            <View
+              style={{
+                position: "absolute",
+                width: 30,
+                height: 30,
+                borderRadius: 15,
+                backgroundColor: `rgb(${
+                  pixels[circlePosition.y][circlePosition.x][0]
+                }, ${pixels[circlePosition.y][circlePosition.x][1]}, ${
+                  pixels[circlePosition.y][circlePosition.x][2]
+                })`,
+                zIndex: 999,
+                left: circlePosition.x - 15,
+                top: circlePosition.y - 15,
+              }}
+            />
+          )}
+          <ImageBackground
+            source={{ uri: photo && photo.uri }}
+            style={{
+              flex: 1,
+              height: "100%",
+            }}
+          />
+        </View>
+      </TouchableWithoutFeedback>
+    </View>
   );
 };
