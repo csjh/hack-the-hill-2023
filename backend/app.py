e347db72a9a38e9cb567224f754d37e216452a98
csjh <cormick.holland@gmail.com>
2023-03-05 03:18:38 -0500

    please work

--- backend/app.py
+++ backend/app.py
@@ -3,12 +3,12 @@ import base64
 import requests
 from io import BytesIO
 from PIL import Image
-from imageai.Detection import ObjectDetection
+# from imageai.Detection import ObjectDetection
 
-detector = ObjectDetection()
-detector.setModelTypeAsYOLOv3()
-detector.setModelPath("../yolov3.pt")
-detector.loadModel()
+# detector = ObjectDetection()
+# detector.setModelTypeAsYOLOv3()
+# detector.setModelPath("../yolov3.pt")
+# detector.loadModel()
 
 app = flask.Flask(__name__)
 
@@ -20,23 +20,25 @@ def get_pixel():
     print("made it to the request")
     image = flask.request.json['image']
     image = Image.open(BytesIO(base64.b64decode(image)))
-    pixels = [[list(image.getpixel((x, y))) for x in range(image.width)]
-              for y in range(image.height)]
+    print(image.width, image.height)
+    pixels = [[list(image.getpixel((x, y))) 
+               for x in range(image.width)]
+               for y in range(image.height)]
     return str(pixels)
 
 
-@app.route('/get_objects', methods=['POST'])
-def index():
-    image = flask.request.json['image']
-    image = base64.b64decode(image)
-    image = BytesIO(image)
-    detections = detector.detectObjectsFromImage(
-        input_image=Image.open(image), output_image_path="imagenew.jpg", minimum_percentage_probability=30)
+# @app.route('/get_objects', methods=['POST'])
+# def index():
+#     image = flask.request.json['image']
+#     image = base64.b64decode(image)
+#     image = BytesIO(image)
+#     detections = detector.detectObjectsFromImage(
+#         input_image=Image.open(image), output_image_path="imagenew.jpg", minimum_percentage_probability=30)
 
-    result = ""
-    for eachObject in detections:
-        result += eachObject["name"] + " : " + \
-            str(eachObject["percentage_probability"]) + \
-            " : " + str(eachObject["box_points"])
-        result += "\n--------------------------------\n"
-    return result
+#     result = ""
+#     for eachObject in detections:
+#         result += eachObject["name"] + " : " + \
+#             str(eachObject["percentage_probability"]) + \
+#             " : " + str(eachObject["box_points"])
+#         result += "\n--------------------------------\n"
+#     return result
